308b71dfd2947d0556c4d4021dc724fc77cd1792
Julian Wiesler <wiesleju@gmail.com>
2022-02-05 15:18:39 +0100

    Rework logging interface

--- src/util/log/mod.rs
+++ src/util/log/mod.rs
@@ -9,6 +9,9 @@ pub use self::flag::Flags;
 use ffi::*;
 use std::convert::TryInto;
 
+#[cfg(all(target_arch = "x86_64", any(target_family = "windows", target_family = "unix")))]
+pub mod callback;
+
 pub fn set_level(value: Level) {
     unsafe { av_log_set_level(value.into()) }
 }
@@ -23,58 +26,4 @@ pub fn set_flags(value: Flags) {
 
 pub fn get_flags() -> Flags {
     unsafe { Flags::from_bits_truncate(av_log_get_flags()) }
-}
-
-pub use libc::c_int;
-pub use libc::c_char;
-
-pub type Context = *mut libc::c_void;
-
-#[cfg(all(target_arch = "x86_64", target_family = "windows"))]
-pub type Args = sys::va_list;
-
-#[cfg(all(target_arch = "x86_64", target_family = "unix"))]
-pub type Args = *mut sys::__va_list_tag;
-
-/// Type of a log callback
-pub type Callback = unsafe extern "C" fn(context: Context, level: c_int, fmt: *const c_char, args: Args);
-
-/// Sets a log callback
-pub fn set_raw_callback(callback: Callback) {
-    unsafe {
-        sys::av_log_set_callback(Some(callback));
-    };
-}
-
-/// Calls the default log function of ffmpeg
-pub unsafe extern "C" fn default_unsafe_callback(context: Context, level: c_int, fmt: *const c_char, args: Args) {
-    sys::av_log_default_callback(context, level, fmt, args);
-}
-
-/// Uses the log crate
-/// Use the feature `respect-ffmpeg-log-level` to control whether the log level set by ffmpeg will be checked before outputting
-pub unsafe extern "C" fn default_callback(_context: Context, level: c_int, fmt: *const c_char, args: Args) {
-    let level = level.try_into().unwrap_or(Level::Info);
-    #[cfg(feature = "respect-ffmpeg-log-level")]
-    if let Ok(enabled_level) = get_level() {
-        if level < enabled_level {
-            return;
-        }
-    }
-
-    let level_filter = match level {
-        Level::Quiet => log::LevelFilter::Off,
-        Level::Trace => log::LevelFilter::Trace,
-        Level::Debug | Level::Verbose => log::LevelFilter::Debug,
-        Level::Info => log::LevelFilter::Info,
-        Level::Warning => log::LevelFilter::Warn,
-        Level::Error | Level::Fatal | Level::Panic => log::LevelFilter::Error,
-    };
-
-    if let Some(log_level) = level_filter.to_level() {
-        if log::log_enabled!(log_level) {
-            let log = vsprintf::vsprintf(fmt, args).expect("failed to format ffmpeg log message");
-            log::log!(target: "ffmpeg", log_level, "{}", log.trim());
-        }
-    }
 }
